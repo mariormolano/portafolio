43a89fe936d848f4fbfa5adef8abe5521b61566f
Mario Molano <mariormolano@yahoo.com>
2023-07-13 18:46:47 -0500

    Portafolio

---
+++ query.js
@@ -0,0 +1,32 @@
+//cargar primero el json-server -w backend/skills.json
+
+//Enlace al componente HTML
+const skillsContainer = document.getElementById("skillsContainer");
+
+//Función flecha sincrónica 
+
+const datos = async (type) => {
+  let data;
+  //solicitar datos de la api
+  await fetch(`http://localhost:8000/${type}`)
+  // solicitar el JSON
+    .then((peticion) => peticion.json())
+  // Guardar la respuesta en una variable
+    .then((respueta) => {(data = respueta)});
+  // retornar la respuesta
+  return data;
+};
+
+//se ejecuta la función y espera a que se retorne los datos
+const skills = await datos("skills");
+
+//iteración con los elementos recibidos
+skills.forEach((element) => {
+  console.log(element);
+
+  skillsContainer.innerHTML =
+    skillsContainer.innerHTML +
+    `<div class="card"><img class="card--icon" src="${element.icon}" /> <p class="card--titulo">${element.skill}</p> </div> `;
+});
+
+
