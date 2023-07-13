c91b745fd2512b31e18cfc4141b4ea9315df6cc8
Mario Molano <mariormolano@yahoo.com>
2023-07-13 18:56:24 -0500

    update server

--- api/server.js
+++ api/server.js
@@ -7,7 +7,7 @@ const middlewares = jsonServer.defaults()
 server.use(middlewares)
 // Add this before server.use(router)
 server.use(jsonServer.rewriter({
-    '/api/*': '/data/$1',
+    '/api/*': '/$1',
     '/blog/:resource/:id/show': '/:resource/:id'
 }))
 server.use(router)
